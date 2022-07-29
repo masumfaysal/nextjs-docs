8f01044a2a6f3af0d5e4e632f5b3940e04bd43c0
masumfaysal <faysal@cdda.io>
2022-07-29 17:06:06 +0600

    1

--- theme.config.js
+++ theme.config.js
@@ -62,6 +62,6 @@ export default {
   nextLinks: true,
   footer: true,
   footerEditLink: 'Edit this page on GitHub',
-  footerText: <>MIT {new Date().getFullYear()} © Nextra.</>,
+  footerText: <> {new Date().getFullYear()} © CDDA.</>,
   unstable_faviconGlyph: '👋',
 }
